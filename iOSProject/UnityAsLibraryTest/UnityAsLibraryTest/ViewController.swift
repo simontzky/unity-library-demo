49b64d0d5f604c9ad083daa7b1fc294377bc0812
Martin Novak <martin@wolargames.com>
2020-03-27 17:15:42 +0100

    Updated project to 2019.3, added iOS native project

---
+++ iOSProject/UnityAsLibraryTest/UnityAsLibraryTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnityAsLibraryTest
+//
+//  Created by Martin Novak on 27/03/2020.
+//  Copyright © 2020 STRV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
