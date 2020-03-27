bbdd190d5de796eeac35cedc67ef0d2919f667ff
Martin Novak <martin@wolargames.com>
2020-03-27 19:11:46 +0100

    Implemented example, removed SceneDelegate

--- iOSProject/UnityAsLibraryTest/UnityAsLibraryTest/AppDelegate.swift
+++ iOSProject/UnityAsLibraryTest/UnityAsLibraryTest/AppDelegate.swift
@@ -11,7 +11,7 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     // MARK: UISceneSession Lifecycle
-
+/*
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
@@ -32,6 +32,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+*/
 }
 
--- iOSProject/UnityAsLibraryTest/UnityAsLibraryTest/ViewController.swift
+++ iOSProject/UnityAsLibraryTest/UnityAsLibraryTest/ViewController.swift
@@ -8,13 +8,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// More info here
+// https://docs.unity3d.com/Manual/UnityasaLibrary-iOS.html
+// https://www.linkedin.com/pulse/how-embed-unity-framework-ios-app-objective-c-swift-malav-soni-/
+// https://github.com/Unity-Technologies/uaal-example
+// https://github.com/Unity-Technologies/uaal-example/blob/master/docs/ios.md
 
+class ViewController: UIViewController
+{
+    var unityFramework:UnityFramework?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        closeButton.removeFromSuperview()
     }
 
+    @IBOutlet var closeButton: UIButton!
 
+    @IBAction func Open(_ sender: Any) {
+        initAndShowUnity()
+    }
+    
+    @IBAction func Close(_ sender: Any)
+    {
+        unityFramework?.unloadApplication()
+        (UIApplication.shared.delegate as! AppDelegate).window?.makeKeyAndVisible()
+    }
+    
+    func initAndShowUnity() -> Void
+    {
+        if let framework = self.unityFrameworkLoad(){
+            self.unityFramework = framework
+            self.unityFramework?.setDataBundleId("com.unity3d.framework")
+            self.unityFramework?.runEmbedded(withArgc: CommandLine.argc, argv: CommandLine.unsafeArgv, appLaunchOpts: [:])
+            self.unityFramework?.showUnityWindow()
+        }
+        
+        self.unityFramework?.appController()?.rootView.addSubview(closeButton)
+    }
+    
+    func unityFrameworkLoad() -> UnityFramework?
+    {
+        let bundlePath = Bundle.main.bundlePath.appending("/Frameworks/UnityFramework.framework")
+        if let unityBundle = Bundle.init(path: bundlePath){
+            if let frameworkInstance = unityBundle.principalClass?.getInstance(){
+               return frameworkInstance
+           }
+        }
+        return nil
+    }
 }
 
+extension UIView {
+    func pinEdges(to other: UIView) {
+        leadingAnchor.constraint(equalTo: other.leadingAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: other.trailingAnchor).isActive = true
+        topAnchor.constraint(equalTo: other.topAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: other.bottomAnchor).isActive = true
+    }
+}
